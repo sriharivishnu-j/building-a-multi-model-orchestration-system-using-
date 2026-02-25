639d0890dedd0d4e2fda9c97259bce8f17ee2249
Sriharivishnu Janakiraman <j.sriharivishnu@gmail.com>
2026-02-26 01:28:11 +0800

    Add orchestration-go/main.go

---
+++ orchestration-go/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.Info("Starting Orchestration Service")
+	// Add orchestration logic here
+	log.Println("Orchestration Service Running")
+}
